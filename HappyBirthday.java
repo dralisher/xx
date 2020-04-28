c63a2f6f7abc0153c6a019753d3d939de8585d40
Alisir Sahin <57968622+dralisher@users.noreply.github.com>
2020-04-28 15:12:40 -0500

    Update HappyBirthday.java

--- HappyBirthday.java
+++ HappyBirthday.java
@@ -3,5 +3,6 @@ package New;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy birth");
+         System.out.println("Happy birth");
     }
 }
